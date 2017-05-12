cd2d1168c18a7d732d693d85109ae5fe21e439b9
Pedro Martinez Carrión <pedromcarrion@gmail.com>
2017-05-12 17:21:28 +0200

    Fines en métodos knew para que funcione con async

--- app/infrastructure/placesrepository.js
+++ app/infrastructure/placesrepository.js
@@ -12,7 +12,7 @@ class PlacesRepository{
         this.database = database;
     }
 
-    async getKenex(){
+    getKenex(){
         return new Knex({
                         client: 'mysql',
                         connection: {
@@ -27,58 +27,58 @@ class PlacesRepository{
     
 
     async getUser(userId){
-        return new Promise((resolve, reject) => {
+
+        try {
+            let knex = this.getKenex();
+            let rows = await knex.columns('id_user','name').where({id_user: userId}).select().from('users');
+            let user;
             
-            try {
-                let knex = this.getKenex();
-                let rows = await(knex.columns('id_user','name').where({id_user: userId}).select().from('users'));
-                
-                if(rows.length > 0){
-                    return resolve(new User(rows[0].id_user, rows[0].name));
-                } else { 
-                    return reject('El usuario no existe en la BBDD, id: ' + userId);
+            if(rows.length > 0){
+                user = new User(rows[0].id_user, rows[0].name);
+            }
+
+            return new Promise((resolve, reject) => {
+                if(user){
+                    resolve(user);
+                }else{
+                    reject('El usuario no existe en la BBDD, id: ' + userId);
                 }
-            }
-            catch(err) {
-                console.log('Error en el método getUser de PlacesRepository',error);
-                return reject(error);
-            }
-            finally{
-                knex.destroy();
-            }
-        });
+            });
+        }
+        catch(err) {
+            console.log('Error en el método getUser de PlacesRepository',err);
+        }
+        finally{
+            knex.destroy();
+        }
+
+        
     }
 
-    getPlaces(userId){
-        
+    async getPlaces(userId){
 
-        return new Promise((resolve, reject) => {
-
+        try{
             let knex = this.getKenex();
-            let rows = await(knex.columns('link','date','description', 'status','image','category','name','longitude','latitude')
+            let rows = await knex.columns('link','date','description', 'status','image','category','name','longitude','latitude')
             .where({id_user: userId})
             .select()
-            .from('places').innerJoin('places_base','places.id_place_base','places_base.id_place_base'));
+            .from('places').innerJoin('places_base','places.id_place_base','places_base.id_place_base');
+            let places = [];
 
-            try{
-                var places = [];
-
-                for(let i = 0, length = rows.length; i < length; i++){
-                    places.push(new Place(rows[i].link, rows[i].date, rows[i].description, rows[i].status, rows[i].image, rows[i].category
-                    , rows[i].name, rows[i].longitude, rows[i].latitude));
-                }
-            
-                return resolve(places);
+            for(let i = 0, length = rows.length; i < length; i++){
+                places.push(new Place(rows[i].link, rows[i].date, rows[i].description, rows[i].status, rows[i].image, rows[i].category
+                , rows[i].name, rows[i].longitude, rows[i].latitude));
             }
-            catch(err){
-                console.log('Error en el método getPlaces de PlacesRepository',error);
-                return reject(error);
-            }
-            finally{
-                 knex.destroy();
-            }
-        });
-        
+            return new Promise((resolve, reject) => {
+                resolve(places);
+            });
+        }
+        catch(err){
+            console.log('Error en el método getPlaces de PlacesRepository',err);
+        }
+        finally{
+            knex.destroy();
+        } 
     }       
 
 }
